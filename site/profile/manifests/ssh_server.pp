7171d4bd80afe1a961ee0fbed8073112a0596a67
hamlil <hampus.lilliehook@gmail.com>
2022-06-27 14:24:48 +0200

    Update ssh_server.pp

--- site/profile/manifests/ssh_server.pp
+++ site/profile/manifests/ssh_server.pp
@@ -10,6 +10,6 @@ class profile::ssh_server {
 		ensure => present,
 		user   => 'root',
 		type   => 'ssh-rsa',
-		key    => '',
+		key    => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQCYiVD2NfcIUrd3XRnA0x/7Tis4xvvUxYLMbbn1wdYzQNd35RdVwIPlH2wLsqtVmkYZ5LbY583ryOA+sj8uSzMGaJg3Q7OvK0hYsPWX2QgV7g/V1J5b1Ug1x0FHHaUvMnlQvRRjXappSuYzQ/oDsd4GdSbk9FrLWGYdN6NntRAyneedmCvfmc9rXgDFz/R6Z90Bi/La5S2o1te3v8bT3jcsLtHaXrsfkC7pPPOSUT0VT3qi4tQn5e5HWd6SxlolDdRkJhM+slzxcSlEbDVUXAcuWOz1CTOM3gzdDRiM7yscBOY9gCinRk/G8R+TEaFXaQnSkME0VvtzCYnEwR3dKhJf',
 	}  
 }
